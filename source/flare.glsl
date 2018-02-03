68a6a0667a9ba45760f756e65e3836bd70b286b8
arxii <yurisido@gmail.com>
2018-02-02 18:16:24 -0600

    small update

--- source/flare.glsl
+++ source/flare.glsl
@@ -1,16 +1,11 @@
 precision mediump float;
 uniform float iTime;
-
-#define PI 3.14159265359
-#define TWO_PI 6.28318530718
-// #define SQ3 1.73205080757
-// #define I_R 0.6
-// #define F_R 9.6
-
 varying vec2 v_uv;
 
 #define PI 3.14159265359
 #define TWO_PI 6.28318530718
+
+
 
 float random (in vec2 _st) { 
     return fract(sin(dot(_st.xy, vec2(12.9898,78.233))) * 43758.54531237);
@@ -43,21 +38,21 @@ float light(in vec2 pos,in float size,in float radius,in float inner_fade,in flo
 }
 
 float flare(in float angle,in float alpha,in float time){
-	float t = time*0.3;
+	float t = time;
 	float n = noise(vec2(t+angle+pow(alpha,0.5),t-angle+pow(alpha,0.5))*6.0);
 	// float n = 0.5;
 	
-    float split = angle*(10.0+sin(t*10.0+n*5.0+angle*15.0+alpha*2.0*n)*0.5);
-	
-    float rotate = sin(t+alpha*20.0 + n*1.5)+angle*9.5*(1.5+0.2*n);
+    float split = (10.0+sin(n*4.0+angle*20.0+alpha*4.0*n)*alpha*1.9);
+
+    float rotate = sin(t*2.0+angle*10.0+alpha*20.0 + n*1.5)+angle*2.5*(1.5+0.2*n);
 	
     
-    float g = pow((2.0+sin(split+n*4.0*alpha+rotate)*1.4)*(1.0+n*4.0),n*1.8);
+    float g = pow((2.0+sin(split+n*2.5*alpha+rotate)*1.4)*(1.0+n*3.5),0.1+n*(1.2-0.6*alpha));
 	
 
-	g *= pow(((angle+PI) * (TWO_PI - angle - PI)),1.2)*0.01; //fade out at pole	
+	g *= pow((angle+PI)*(TWO_PI - angle - PI),0.45)*0.2; //fade out at pole	
 	g *= alpha * alpha * alpha;
-	g += alpha + g * 0.7;
+	g += alpha + g * g;
 	return g;
 }
 
@@ -66,31 +61,39 @@ float flare(in float angle,in float alpha,in float time){
 
 
 #define SIZE 1.0
-#define RADIUS 0.1
+#define RADIUS 0.09
 #define INNER_FADE .81
 #define OUTER_FADE 0.02
+#define SPEED 0.2
 
-
-
-#define ECLIPSE_SMOOTH 0.05
+#define BORDER 0.091
 
 
 
 
 void main() {
-	vec2 uv = vec2(v_uv.x-0.5,v_uv.y-0.5);
+	vec2 uv = -vec2(v_uv.x-0.5,v_uv.y-0.5);
 	float f = 0.0;
+    float f2 = 0.0;
+    float t = iTime * SPEED;
 	float alpha = light(uv,SIZE,RADIUS,INNER_FADE*1.5,OUTER_FADE*4.0);
 	float angle = atan(uv.x,uv.y);
-	
-	if(alpha >= 0.999){
-		alpha = (1.0 - light(uv,SIZE,0.0,INNER_FADE*0.725,OUTER_FADE))*1.8;
-		f = flare(angle,alpha*0.7,iTime+0.5);
-        f *= 1.1;
+	float l = length(uv);
+	if(l < BORDER){
+        alpha = (1.0 - pow(((BORDER - l)*2.2/BORDER)*4.0,0.15)*0.65)*1.46;
+        if(l<BORDER/1.75){
+          //  float n = noise(vec2(t*2.0+uv.x*30.0,t*2.0+uv.y*30.0));
+        	alpha -= pow((l - BORDER/1.75)*5.4,2.6)*12.5;
+        }
+        alpha = clamp(alpha,.0,1.);
+        //f = alpha
+        f = flare(angle,alpha*0.7,t+0.5)*1.8;
+        f2 = flare(angle,alpha*1.3,((t+.5)*1.1))*0.5;
+     	
 	}else if(alpha < 0.001){
 		f = alpha;
 	}else{
-		f = flare(angle,alpha,iTime)*1.8;
+		f = flare(angle,alpha,t)*1.3;
 	}
-	gl_FragColor = vec4(vec3(f,f*alpha,alpha),1.0);
+	gl_FragColor = vec4(vec3(f+f2*f2*f2,f*alpha+f2*f2*2.0,f*alpha*0.50+f2*1.6)*(.85),1.0);
 }
